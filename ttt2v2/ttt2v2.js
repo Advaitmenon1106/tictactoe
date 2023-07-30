3a1a7c2e5726621c4f0dcd6433420d8826da0f45
advait0603 <advaitlinux11@gmail.com>
2023-07-31 01:09:07 +0530

    Implemented an adjacency matrix-like 2d Array that gives a numeric layout of the TicTacToe board and keeps track of the boxes that are marked with an X or O with the respective integers assigned to each (1 for X, 2 for 0).

--- ttt2v2/ttt2v2.js
+++ ttt2v2/ttt2v2.js
@@ -1,12 +1,17 @@
 const gameStackOperations = (()=>{
-    const initialise = (arr)=>{
+    const initialise = (arr, arr2)=>{
         for (i = 0; i<=3; i++){
             arr.push('X');
             arr.push('O');
         }
         arr.push('X');
-    }
 
+        for (i = 0; i<3; i++){
+            for (j = 0; j<3; j++){
+                arr2[i][j];
+            }
+        }
+    }
     const removeMove = (arr)=>{
         arr = arr.splice(0,1);
     }
@@ -15,10 +20,12 @@ const gameStackOperations = (()=>{
 
 const tttBoard = (()=>{
     const generate = ()=>{
+        document.body.style.backgroundColor = 'black';
         let grid = document.createElement('div');
-        grid.style.cssText = 'display: grid; width: fit-content; margin-left: 25%; margin-top: 2%; background-color: black';
+        grid.style.cssText = 'display: grid; width: fit-content; margin-left: 25%; margin-top: 1.5%; background-color: black';
         grid.style.gridTemplateRows = '300px 300px 300px';
         grid.style.gridTemplateColumns = '300px 300px 300px';
+        grid.id = 'outerBox'
         document.body.appendChild(grid);
         for (i = 0; i<9; i++){
             let gridUnit = document.createElement('div');
@@ -26,18 +33,73 @@ const tttBoard = (()=>{
             gridUnit.style.cssText = 'height: 300px; width: 300px; border: 3px solid white; color: white; text-align: center';
             gridUnit.style.fontSize = '290px';
             grid.appendChild(gridUnit);
+            gridUnit.style.cursor='pointer'
+            gridUnit.id = i.toString();
+            // gridUnit.addEventListener('mouseenter', ()=>{
+            //     gridUnit.style.backgroundColor = 'silver';
+            // })
         }
+
     };
-    return {generate};
+    const reset = ()=>{
+        gameStackOperations.initialise(moveStack);
+        generate();
+        gameplay.playATurn();
+    }
+    return {generate, reset};
     
 })();
 
-let moveStack = [];
+const gameplay = (()=>{
+    const playATurn = (arr)=>{
+        let boxes = document.getElementsByClassName('tttBoxes');
+        for (i = 0; i<box.length; i++){
+            boxes[i].addEventListener('click', (event)=>{
+                if (event.target.markedOrNot==false){
+                    event.target.style.color = 'white';
+                    event.target.innerHTML = moveStack[0];
+                    gameStackOperations.removeMove(moveStack);
+                    event.target.markedOrNot=true;
+                    if (moveStack[0] == 'O'){
+                        arr[Math.floor(parseInt(event.target.id)/3)][parseInt(event.target.id)%3] = 1;//1, if character on the board is X
+                    }
+                    else if (moveStack[0] == 'X'){
+                        arr[Math.floor(parseInt(event.target.id)/3)][parseInt(event.target.id)%3] = 2;//2, if character on the board is O
+                    }
+                }
+                else{
+                    return;
+                }
+            })
+        }
+    }
+    const win = (adjMat)=>{
+        //Code for the win condition
+    }
+    return {playATurn}
+})()
 
-window.onload = ()=>{
-    tttBoard.generate();
-    gameStackOperations.initialise(moveStack);
+let moveStack = [];
+let adjacencyMatrix = [[], [], []];
+
+tttBoard.generate();
+gameStackOperations.initialise(moveStack, adjacencyMatrix);
+let box = document.getElementsByClassName('tttBoxes');
+
+for (i = 0; i<box.length; i++){
+    Object.defineProperty(box[i], 'markedOrNot', {
+        value: false,
+        writable: true
+    })
 }
 
-let box = document.getElementsByClassName('tttBoxes');
-// Object.defineProperty(box[0]) -> continue by adding a marked or not property
+gameplay.playATurn(adjacencyMatrix);
+
+/*let resetButton = document.getElementById('reset');
+resetButton.addEventListener('click', ()=>{
+    for (i = 0; i<box.length; i++){
+        box[i].innerHTML = null;
+    }
+})*/
+
+//Strategy for 3 in a row: match indices of 2d array and id%3 of a grid unit
